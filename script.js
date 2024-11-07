cd496e514543cc5313f1fbf6053f8f998be144b4
Codefitness21 <kelly.m.hill2115@gmail.com>
2024-11-06 20:42:36 -0600

    changed the blinker interval time to 1000 from 1200.

--- script.js
+++ script.js
@@ -57,4 +57,4 @@ function goRight() {
                 let blink = document.getElementById('blink');
                 setInterval(function () {
                     blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
-                }, 1200);
+                }, 1000);

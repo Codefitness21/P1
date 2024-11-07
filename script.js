3ac3d7c4e48dbedd7eb367d1cca49c5dee2faafd
Codefitness21 <kelly.m.hill2115@gmail.com>
2024-11-06 20:40:49 -0600

    updated js blinker. Switched to let instead of var.

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function goRight() {
             }*/
 
 
-                var blink = document.getElementById('blink');
+                let blink = document.getElementById('blink');
                 setInterval(function () {
                     blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
                 }, 1200);
